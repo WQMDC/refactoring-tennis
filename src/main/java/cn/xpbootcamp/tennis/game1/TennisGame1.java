8c21342e631c557036923e208db8a3dd024dba52
WQM <244401893@qq.com>
2020-03-05 00:14:14 +0800

    refactor if else

--- src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
+++ src/main/java/cn/xpbootcamp/tennis/game1/TennisGame1.java
@@ -20,7 +20,6 @@ public class TennisGame1 implements TennisGame {
 
     public String getScore() {
         String score = "";
-        int tempScore = 0;
         if (m_score1 == m_score2) {
             switch (m_score1) {
                 case 0:
@@ -39,14 +38,20 @@ public class TennisGame1 implements TennisGame {
             }
         } else if (m_score1 >= 4 || m_score2 >= 4) {
             int minusResult = m_score1 - m_score2;
-            if (minusResult == 1) score = "Advantage player1";
-            else if (minusResult == -1) score = "Advantage player2";
-            else if (minusResult >= 2) score = "Win for player1";
-            else score = "Win for player2";
+            if (minusResult == 1) {
+                score = "Advantage player1";
+            } else if (minusResult != -1) {
+                if (minusResult >= 2) score = "Win for player1";
+                else score = "Win for player2";
+            } else {
+                score = "Advantage player2";
+            }
         } else {
+            int tempScore = 0;
             for (int i = 1; i < 3; i++) {
-                if (i == 1) tempScore = m_score1;
-                else {
+                if (i == 1) {
+                    tempScore = m_score1;
+                } else {
                     score += "-";
                     tempScore = m_score2;
                 }
